dc45bd45771166067df01d0d79275b34f0ae07f6
cyberplanner <saleh.gfx@gmail.com>
2016-08-03 17:05:03 +0100

    Finished user request 2, 3. tests passed.

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -9,6 +9,17 @@ describe('Thermostat' , function(){
   });
 
   it('starts at 20 degrees', function(){
-    expect(thermostat.temperature).toEqual(20);
+    expect(thermostat.currentTemperature()).toEqual(20);
   });
+
+  it('increases temperature using up button', function() {
+    thermostat.increasesTemperature();
+    expect(thermostat.currentTemperature()).toEqual(21);
+  });
+
+  it('decreases temperature with up button', function() {
+    thermostat.decreasesTemperature();
+    expect(thermostat.currentTemperature()).toEqual(19);
+  });
+  
 });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -3,3 +3,15 @@
 function Thermostat() {
   this.temperature = 20;
 };
+
+Thermostat.prototype.currentTemperature = function () {
+  return this.temperature
+};
+
+Thermostat.prototype.increasesTemperature = function () {
+  this.temperature += 1;
+};
+
+Thermostat.prototype.decreasesTemperature = function () {
+  this.temperature -= 1;
+};
