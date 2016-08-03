3c74d9ed07e67c36da97b07466516d0a403ac877
samjbro <sjbroughton1@gmail.com>
2016-08-03 19:23:47 +0100

    adds local temperature using Open Weather Map API

--- interface.js
+++ interface.js
@@ -1,4 +1,9 @@
 $(document).ready(function(){
+
+  $.get('http://api.openweathermap.org/data/2.5/weather?q=London&appid=bcebb95329f7ace43020439afcb4e214&units=metric', function(data) {
+    $('#current-temperature').text(data.main.temp);
+  })
+
   var thermostat = new Thermostat();
   updateTemperature();
 
