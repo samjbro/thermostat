ab2f3e840891f4dd131bfb5a9e6a3844d19b2ef0
samjbro <sjbroughton1@gmail.com>
2016-08-04 14:37:04 +0100

    adds local weather API

--- interface.js
+++ interface.js
@@ -32,5 +32,34 @@ $(document).ready(function(){
     $("#power-saving-status").text("off");
   });
 
+  $("#select-city").submit(function(event){
+    event.preventDefault();
+    var city = $("#current-city").val();
+    displayWeather(city);
+  });
+
+  displayWeather('London');
+
+  function displayWeather(city){
+    var url = 'http://api.openweathermap.org/data/2.5/weather?q=';
+    var token = '&appid=bcebb95329f7ace43020439afcb4e214';
+    var units = '&units=metric';
+    $.get(url + city + token + units, function(data) {
+     $("#current-temperature").text(data.main.temp);
+    });
+    $("#selected-city").text(city);
+  };
+
 
 });
+
+
+
+
+
+
+
+
+
+
+
